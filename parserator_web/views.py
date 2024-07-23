efe9a674195b70a1ee6f2fbb712d586f09bd3d38
Katrina Wheelan <kwheelan@umich.edu>
2024-07-22 21:47:55 -0400

    adjusted error handling to throw ParseError

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -15,7 +15,7 @@ class AddressParse(APIView):
     def get(self, request):
         '''Parse an address string and return the parsed components to the frontend.'''
 
-        # get address from POST request
+        # get address parameter from request
         address = request.query_params.get('address', None)
         if not address:
             data = {
@@ -27,19 +27,26 @@ class AddressParse(APIView):
             }
             return Response(data, status=status.HTTP_400_BAD_REQUEST)
 
-        # Use parse method to get address_components and address_type
-        address_components, address_type = self.parse(address)
-
+        try:
+            # Use parse method to get address_components and address_type
+            address_components, address_type = self.parse(address)
+            data = {
+                'input_string': address,
+                'address_components': address_components,
+                'address_type': address_type,
+                'status': 'success'
+            }
+            return Response(data, status = status.HTTP_200_OK) 
+        
         # if parse returns an error, pass error details to frontend
-        if isinstance(address_components, Exception):
-
+        except ParseError as e:
             # trim error message to make it readable
-            full_message = str(address_components)
+            full_message = str(e)
             if 'ERROR:' in full_message and "ORIGINAL STRING:" in full_message:
                 error_message = full_message.split("ERROR:", 1)[-1].split("ORIGINAL STRING:", 1)[0].strip()
             else:
                 error_message = full_message
-                
+
             # return error message as part of JSON response
             data = {
                 'input_string' : address,
@@ -49,22 +56,13 @@ class AddressParse(APIView):
                 'message' : error_message
             }
             return Response(data, status = status.HTTP_422_UNPROCESSABLE_ENTITY)
-        
-        # if no error, pass expected response
-        else:
-            data = {
-                'input_string': address,
-                'address_components': address_components,
-                'address_type': address_type,
-                'status': 'success'
-            }
-            return Response(data, status = status.HTTP_200_OK)            
+    
 
     def parse(self, address):
         ''' Return the parsed components of a given address using usaddress '''
         try:
             address_components, address_type = usaddress.tag(address)
-            return address_components, address_type
-        # if there's an error, return the exception
-        except Exception as e: 
-            return e, None
+            return address_components, address_type  
+        # Raise ParseError for any other exception
+        except Exception as e:
+            raise ParseError(detail=str(e))  
